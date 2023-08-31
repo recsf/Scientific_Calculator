7ca438919a9738bb9fc65b8f22a9a111e8a4cffb
recsf <79220184+recsf@users.noreply.github.com>
2023-08-31 12:56:33 -0600

    define GUI specifications for the display

--- main.py
+++ main.py
@@ -188,4 +188,9 @@ class Calc:
         self.display(self.current)
 
 
+added_value = Calc()
+txtDisplay = Entry(calc, font=('Helvetica', 20, 'bold'), bg='black', fg='white', bd=30, width=28, justify=RIGHT)
+txtDisplay.grid(row=0, column=0, columnspan=4, pady=1)
+txtDisplay.insert(0, "0")
+
 root.mainloop()
