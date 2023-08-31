fcccae87c9659214bcffe8bfc7be3c5fe5e32f48
recsf <79220184+recsf@users.noreply.github.com>
2023-08-31 13:02:09 -0600

    Made number pad that allow the user give input

--- main.py
+++ main.py
@@ -193,4 +193,15 @@ txtDisplay = Entry(calc, font=('Helvetica', 20, 'bold'), bg='black', fg='white',
 txtDisplay.grid(row=0, column=0, columnspan=4, pady=1)
 txtDisplay.insert(0, "0")
 
+numberpad = "789456123"
+i = 0
+btn = []
+for j in range(2, 5):
+    for k in range(3):
+        btn.append(Button(calc, width=6, height=2, bg='black', fg='white', font=('Helvetica', 20, 'bold'), bd=4,
+                          text=numberpad[i]))
+        btn[i].grid(row=j, column=k, pady=1)
+        btn[i]["command"] = lambda x=numberpad[i]: added_value.numberEnter(x)
+        i += 1
+
 root.mainloop()
